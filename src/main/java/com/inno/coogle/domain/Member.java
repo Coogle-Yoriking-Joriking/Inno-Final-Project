e5e50f6be4f804e666f484603e7eef8d29d4fb88
SoonWookHwang <108779266+SoonWookHwang@users.noreply.github.com>
2022-08-29 17:34:39 +0900

    [#2]Feat: Member 불필요한 @Setter 어노테이션 제거

--- src/main/java/com/inno/coogle/domain/Member.java
+++ src/main/java/com/inno/coogle/domain/Member.java
@@ -3,13 +3,10 @@ package com.inno.coogle.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @Table(name = "member")
 public class Member {
