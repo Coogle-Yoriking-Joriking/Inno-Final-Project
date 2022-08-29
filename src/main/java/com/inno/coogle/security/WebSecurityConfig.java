96d81b5bc58bf66e7903f7e5c83f44c4dd971d97
soonwook <bravadosw@naver.com>
2022-08-29 17:15:32 +0900

    [#2]Feat: 로그인, jwt access token, 인가 설정 변경

--- src/main/java/com/inno/coogle/security/WebSecurityConfig.java
+++ src/main/java/com/inno/coogle/security/WebSecurityConfig.java
@@ -2,104 +2,65 @@ package com.inno.coogle.security;
 
 import com.inno.coogle.jwt.JwtAuthenticationFilter;
 import com.inno.coogle.jwt.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.CorsUtils;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
-@RequiredArgsConstructor
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
+@EnableGlobalMethodSecurity(
+        securedEnabled = true
+)
+public class WebSecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
 
-
-    @Bean //비밀번호 암호화
+    @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    @Override // Bean 에 등록
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    // 정적 자원에 대해서는 Security 설정을 적용하지 않음
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(corsConfigurationSource());
-
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "**/*").permitAll()
-                .antMatchers(HttpMethod.GET, "**/*").permitAll()
-                .antMatchers(HttpMethod.PUT, "**/*").permitAll()
-                .antMatchers(HttpMethod.POST, "**/*").permitAll()
-                .antMatchers(HttpMethod.DELETE, "**/*").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api/login").permitAll()
-//                .antMatchers(HttpMethod.GET, "/api/signup").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/login").permitAll()
-//                .antMatchers(HttpMethod.POST, "/api/signup").permitAll()
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                .antMatchers("**").permitAll()
-//                .antMatchers("/api/signup").permitAll()
-//                .antMatchers("/api/login").permitAll()
-
-                // 그 외 모든 요청은 인증 과정 필요
+                .and()
+                .authorizeRequests()
+                .mvcMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .antMatchers("/posts/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/scripts/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/users/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                // 토큰을 활용하면 세션이 필요없기 때문에 STATELESS 로 설정하여 세션을 사용하지 않는다.
+                // 세션인증 사용하지 않음
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    // CORS 설정 빈 추가
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-
-//        config.addAllowedOrigin("*");
-        config.addAllowedOrigin("http://localhost:3000");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-        config.setAllowCredentials(true);
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE"));
-        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        config.setExposedHeaders(Arrays.asList("Authorization"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
+                // 토큰인증 먼저 실행
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
 
+    public WebSecurityConfig(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 }
