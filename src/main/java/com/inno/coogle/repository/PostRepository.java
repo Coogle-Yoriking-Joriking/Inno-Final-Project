aeb19dc2afb5f82ba7dbfef10ecfa55c92ea54b9
SeonLee1204 <81129200+SeonLee1204@users.noreply.github.com>
2022-09-03 13:38:52 +0900

    [#6]Feat: 검색기능 구현 + 필터기능 추가 (order by 기능 미구현)

--- src/main/java/com/inno/coogle/repository/PostRepository.java
+++ src/main/java/com/inno/coogle/repository/PostRepository.java
@@ -10,6 +10,13 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByModifiedAtDesc();
 
+    //레시피 재료로 검색
     @Query(value = "FROM Post  WHERE UPPER(ingredientsList)  LIKE CONCAT (UPPER(?1) ,'%') ")
     List<Post> findByIngredientsListContainingIgnoreCase(String keyword);
+
+    //레시피 재료와 재외목록을 함께 검색하기
+    @Query(value = "FROM Post  WHERE ingredientsList NOT LIKE CONCAT (?1 ,'%') AND ingredientsList  LIKE CONCAT ('%' , ?2 ,'%') " )
+    List<Post> filterSearch(String except, String keyword);
+
+
 }
